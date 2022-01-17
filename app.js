bf6ca5c962606603a44ce4c690d51664d3963027
cyrus <cyrus7@hotmail.fr>
2022-01-17 15:50:23 +0100

    Feat: boilerplate

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
 const express = require('express');
+var path = require('path');
 const port = 3000;
 var bodyParser = require('body-parser')
 const app = express();
   
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
-    
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
-    
-app.post('/', urlencodedParser, (req, res) => {
-    console.log("From the server:", req.body.data);
-    res.sendStatus(200);
-});
-    
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+app.use(express.static(path.join(__dirname, 'public'))); 
+
+app.post('/', urlencodedParser, (req, res) => {
+    app.use(express.static(__dirname + '/public'));
+    console.log("From the server:", req.body.data);
+});
+
+app.listen(3000);
+console.log('Listening on port 80');
